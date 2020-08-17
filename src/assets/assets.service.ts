5c72a6f6450f5e98ce6af2e6794457de1b0d9482
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2020-08-17 12:22:33 +0200

    thumb creation for images

--- src/assets/assets.service.ts
+++ src/assets/assets.service.ts
@@ -3,15 +3,18 @@ import { InjectModel } from '@nestjs/mongoose';
 import { v4 } from 'uuid';
 import { Model } from 'mongoose';
 import path from 'path';
-import Jimp from 'jimp/es';
+import jimp from 'jimp/es';
 
+import { ConfigService } from '@nestjs/config';
 import { Assetref } from './interfaces/assetref.interface';
 import { AssetrefSubmitDto} from './dto/assetref-submit.dto';
 
 @Injectable()
 export class AssetsService {
+
   constructor(
-    @InjectModel('Asset') private readonly assetRefModel: Model<Assetref>
+    @InjectModel('Asset') private readonly assetRefModel: Model<Assetref>,
+    private readonly configService: ConfigService,
   ) {
   }
 
@@ -21,16 +24,18 @@ export class AssetsService {
    * @param AssetMD
    */
   async submitAsset(fileinfo, AssetMD: AssetrefSubmitDto): Promise<Assetref> {
+    console.log(fileinfo);
     const assetdoc = {
       name: AssetMD.name ? AssetMD.name:fileinfo.originalname,
       identifier: AssetMD.identifier,
       source: AssetMD.source,
       originalname: fileinfo.originalname,
-      path: fileinfo.path,
+      path: fileinfo.filename,
       size: fileinfo.size,
       mimetype: fileinfo.mimetype,
     }
     const asset = new this.assetRefModel(assetdoc);
+    await this.makeImgThumb(fileinfo.filename, {width: 1500, height: 1500}, 90, 'preview');
     return await asset.save();
   }
 
@@ -41,10 +46,8 @@ export class AssetsService {
    * @param callback
    */
   static editFileName(req: any, file: any, callback: any) {
-    //TODO: we might need some more sanitation here?
-    const name = file.originalname.split('.')[0];
     const fileExtName = path.extname(file.originalname);
-    callback(null, `${name}-${v4()}${fileExtName}`);
+    callback(null, `${v4()}${fileExtName}`);
   };
 
 
@@ -52,26 +55,26 @@ export class AssetsService {
     console.log(fileinfo);
   }
 
-/*  makeImgThumb(imgname, dims, qual, thumbname) {
-    return new Promise( function(resolve, reject){
-      console.log(`attempting to create image for ${imgname} in ${CONFIG.assets.thumbs}/${imgname.split('.')[0]}_${thumbname}.jpg`);
-      jimp.read(`${CONFIG.assets.dir}/${imgname}`)
+  async makeImgThumb(imgname, dims, qual, thumbname): Promise<any> {
+    return new Promise( (resolve, reject) => {
+      console.log(`attempting to create image for ${imgname} in ${this.configService.get('assets.dir')}/${imgname.split('.')[0]}_${thumbname}.jpg`);
+      jimp.read(`${this.configService.get('assets.dir')}/${imgname}`)
         .then( img => {
           if(img) {
             if (dims && dims.height && dims.width) img.cover(dims.width, dims.height);
             if (qual) img.quality(qual);
             if (imgname && (dims || qual)) {
-              img.write(`${CONFIG.assets.thumbs}/${imgname.split('.')[0]}_${thumbname}.jpg`, () => {
-                console.log(`image created for ${imgname} in ${CONFIG.assets.thumbs}/${imgname.split('.')[0]}_${thumbname}.jpg`);
-                resolve(`${CONFIG.assets.thumbs}/${imgname.split('.')[0]}_${thumbname}.jpg`);
+              img.write(`${this.configService.get('assets.thumbs')}/${imgname.split('.')[0]}_${thumbname}.jpg`, () => {
+                console.log(`image created for ${imgname} in ${this.configService.get('assets.thumbs')}/${imgname.split('.')[0]}_${thumbname}.jpg`);
+                resolve(`${this.configService.get('assets.thumbs')}/${imgname.split('.')[0]}_${thumbname}.jpg`);
               });
             }
           }
         })
         .catch( err => {
-          console.log(`failed to read image ${CONFIG.assets.dir}/${imgname}`, err);
+          console.log(`failed to read image ${this.configService.get('assets.dir')}/${imgname}`, err);
           reject(err);
         });
     });
-  },*/
+  }
 }
