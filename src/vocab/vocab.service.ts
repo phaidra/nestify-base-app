0f202a4bc83c0113f68fd0703dbc6960409ecef9
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2020-08-18 17:33:41 +0200

    models injected

--- src/vocab/vocab.service.ts
+++ src/vocab/vocab.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Concept } from './Interfaces/concept.interface';
+import { Authrec } from './Interfaces/authrec.interface';
 
 @Injectable()
-export class VocabService {}
+export class VocabService {
+  constructor(
+    @InjectModel('Concept') private readonly conceptModel: Model<Concept>,
+    @InjectModel('Authrec') private readonly authrecModel: Model<Authrec>,
+  ) {
+  }
+
+
+}
