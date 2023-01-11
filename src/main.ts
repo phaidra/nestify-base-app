5e2f813f6a3d6c4daf4a67568c939e771f17faf9
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-01-11 17:16:34 +0100

    major version upgrades

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import { AppModule } from './app.module';
 import { UserModule } from './user/user.module';
 import { SchemasModule } from './schemas/schemas.module';
 import { AssetsModule } from './assets/assets.module';
+import { ConfigService } from '@nestjs/config';
+import {SchemasService} from "./schemas/schemas.service";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(`api/v${app.get('ConfigService').get('version')}`);
-  app.enableCors(app.get('ConfigService').get('cors'));
+  app.setGlobalPrefix(`api/v${app.get(ConfigService).get('version')}`);
+  app.enableCors(app.get(ConfigService).get('cors'));
 
   const options = new DocumentBuilder()
-    .setTitle(app.get('ConfigService').get('meta.title'))
-    .setDescription(app.get('ConfigService').get('meta.description'))
-    .setVersion(app.get('ConfigService').get('version'))
+    .setTitle(app.get(ConfigService).get('meta.title'))
+    .setDescription(app.get(ConfigService).get('meta.description'))
+    .setVersion(app.get(ConfigService).get('version'))
     .addBearerAuth({type: 'http', scheme: 'bearer', bearerFormat: 'JWT'}, 'bearer')
     .build();
   const document = SwaggerModule.createDocument(app, options, {
@@ -26,10 +28,10 @@ async function bootstrap() {
     ],
   });
 
-  app.get('SchemasService').addSwaggerDefs(document);
-  SwaggerModule.setup(`api/v${app.get('ConfigService').get('version')}/swagger`, app, document);
+  app.get(SchemasService).addSwaggerDefs(document);
+  SwaggerModule.setup(`api/v${app.get(ConfigService).get('version')}/swagger`, app, document);
 
-  await app.listen(app.get('ConfigService').get('env.port')).then(() => {
+  await app.listen(app.get(ConfigService).get('env.port')).then(() => {
     console.log('listening');
   });
 }
